3dd17c2312ec34e1bd3d85dd60573bd965480f69
ram ganesh rathour <ramgrathour@gmail.com>
2018-04-13 20:24:23 +0530

    without .class

--- src/string/Concating.java
+++ src/string/Concating.java
@@ -4,7 +4,7 @@ public class Concating {
 	public static void main(String[] args) {
 		String str="abc";
 		String str1="cde";
-		System.out.println(str1+str);
+		System.out.println(str1+str+"abs");
 	}
 
 }
